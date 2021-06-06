5eb9ac41d013b7035e66382c03082dae581f3e09
shenqi.lv <248120694@qq.com>
2021-06-06 23:57:43 +0800

    去掉 render进程中的 ipcRender 调用

--- src/renderer/main_window/index.ts
+++ src/renderer/main_window/index.ts
@@ -1,4 +1,3 @@
-import { ipcRenderer } from 'electron'
 console.log("hello main")
 
 const boxDom = document.querySelector('.electron-version')
@@ -10,11 +9,9 @@ const btnAbout = document.createElement('button')
 btnAbout.innerHTML = '打开about 窗口'
 
 btnHelp.onclick = function(){
-    ipcRenderer.send('openHelpWin')
 }
 
 btnAbout.onclick = function(){
-    ipcRenderer.send('openAboutWin')
 }
 
 boxDom?.appendChild(btnAbout)
