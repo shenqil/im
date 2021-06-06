2b21c521f58a3f7ca4c14d3d3843293649119317
shenqi.lv <248120694@qq.com>
2021-06-06 15:08:23 +0800

    写死主进程编译输出文件名称

--- build/configs/webpack.main.js
+++ build/configs/webpack.main.js
@@ -2,11 +2,11 @@ const { resolve, packageInfo, isProduction } = require('../utils/common')
 
 module.exports = {
     target: `electron${packageInfo.electronVersion}-main`,
-    entry: resolve(`${packageInfo.main}`),
+    entry: resolve('main.ts'),
     mode: isProduction ? 'production' : 'development',
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     output: {
-        filename: '[name].[contenthash:10].js',
+        filename: '[name].built.js',
         path: resolve('.webpack/main'),
     },
     module: {
