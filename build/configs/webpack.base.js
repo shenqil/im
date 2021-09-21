f018d428a580ebd833d9e021f6a28f88b1d077f9
shenqi.lv <248120694@qq.com>
2021-09-21 12:44:05 +0800

    格式化

--- build/configs/webpack.base.js
+++ build/configs/webpack.base.js
@@ -1,35 +1,35 @@
-const { isProduction } = require('../utils/common')
+const { isProduction } = require("../utils/common");
 
 module.exports = {
-    output: {
-        filename: '[name].built.js',
-        chunkFilename: '[name].[contenthash:10].chunk.js'
+  output: {
+    filename: "[name].built.js",
+    chunkFilename: "[name].[contenthash:10].chunk.js",
+  },
+  mode: isProduction ? "production" : "development",
+  devtool: isProduction ? "source-map" : "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.node$/,
+        use: "node-loader",
+      },
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      minSize: 5 * 1024,
     },
-    mode: isProduction ? 'production' : 'development',
-    devtool: isProduction ? 'source-map' : 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.node$/,
-                use: 'node-loader',
-            },
-            {
-                test: /\.(ts|js)x?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-            },
-        ],
-    },
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
-            minSize: 5 * 1024
-        },
-    },
-    watchOptions: {
-        ignored: '**/node_modules',
-    },
-    resolve: {
-        extensions: ['.js', '.ts', '.jsx', '.tsx', '.json']
-    },
-}
+  },
+  watchOptions: {
+    ignored: "**/node_modules",
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".json"],
+  },
+};
