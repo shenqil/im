329c75b33928cef3526383788d44b3bb410831f7
shenqi.lv <shenqi.lv@31huiyi.com>
2021-12-27 22:00:50 +0800

    增加 ConversationSrv

--- src/main/modules/sqlite3/interface.ts
+++ src/main/modules/sqlite3/interface.ts
@@ -1,7 +1,10 @@
 import { ISQ3Common } from './common';
+import { ISQ3Conversation } from './conversation';
 
 export * from './common';
+export * from './conversation';
 
 export default interface ISQ3{
-  common:ISQ3Common
+  common:ISQ3Common,
+  conversation:ISQ3Conversation
 }
---
+++ src/main/server/conversationSrv.ts
@@ -0,0 +1,51 @@
+import SQ3 from '@main/modules/sqlite3';
+import type { IConversationInfo } from '@main/modules/sqlite3/interface';
+import userSrv from './userSrv';
+
+export interface IConversationSrv {
+  get():Promise<Readonly<IConversationInfo[]>>,
+  set(list:IConversationInfo[]):Promise<unknown>
+}
+
+class ConversationSrv implements IConversationSrv {
+  private userId:string;
+
+  private list:IConversationInfo[];
+
+  constructor() {
+    this.userId = '';
+    this.list = [];
+  }
+
+  private async check():Promise<unknown> {
+    const userInfo = await userSrv.getUserInfo();
+
+    if (this.userId !== userInfo.id) {
+      this.list = [];
+      this.userId = userInfo.id;
+    }
+
+    return '';
+  }
+
+  async get():Promise<Readonly<IConversationInfo[]>> {
+    await this.check();
+
+    if (!this.list.length) {
+      this.list = await SQ3.conversation.get(this.userId);
+    }
+
+    return this.list;
+  }
+
+  async set(list: IConversationInfo[]): Promise<unknown> {
+    await this.check();
+
+    await SQ3.conversation.save(this.userId, list);
+
+    this.list = list;
+    return '';
+  }
+}
+
+export default new ConversationSrv();
