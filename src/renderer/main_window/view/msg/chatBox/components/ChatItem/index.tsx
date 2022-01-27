fc0e476d5f50a60dc58dc0b39aed2fd67610628a
shenqi.lv <shenqi.lv@31huiyi.com>
2022-01-27 18:28:48 +0800

    修正 单聊头像显示

--- src/renderer/main_window/view/msg/chatBox/components/ChatItem/index.tsx
+++ src/renderer/main_window/view/msg/chatBox/components/ChatItem/index.tsx
@@ -1,5 +1,7 @@
-import React, { FC } from 'react';
-import { IMessage, EMsgType } from '@main/interface/msg';
+import React, { FC, useMemo } from 'react';
+import { IMessage, EMsgType, ECharType } from '@main/interface/msg';
+import { useAppSelector } from '@renderer/main_window/store/hooks';
+import { selectFriendList } from '@renderer/main_window/store/friend';
 import Avatar from '@renderer/main_window/components/Avatar';
 import { IUserInfo } from '@main/modules/mqtt/interface';
 import TextMsg from '../TextMsg';
@@ -7,11 +9,25 @@ import './index.scss';
 
 export interface IChatItemProps {
   msg:IMessage,
-  userInfo:IUserInfo
+  userInfo:IUserInfo,
 }
 const ChatItem:FC<IChatItemProps> = function (props) {
   const { msg, userInfo } = props;
+  const friendList = useAppSelector(selectFriendList);
   const isSelf = userInfo.id === msg.formId;
+
+  const avatar = useMemo(() => {
+    if (userInfo.id === msg.formId) {
+      return userInfo.avatar;
+    }
+
+    if (msg.charType === ECharType.single) {
+      const friendInfo = friendList.find((item) => item.id === msg.formId);
+      return friendInfo?.avatar || '';
+    }
+
+    return '';
+  }, [friendList, userInfo, msg]);
 
   function getCardId() {
     if (userInfo.id === msg.toId) {
@@ -25,7 +41,7 @@ const ChatItem:FC<IChatItemProps> = function (props) {
     <div className={`chat-item ${isSelf && 'chat-item--self'}`}>
       <div className="chat-item__avatar">
         <Avatar
-          url={userInfo.avatar}
+          url={avatar}
           cardId={getCardId()}
         />
       </div>
